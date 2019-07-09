db5ff94cc6d5776d6096f1431bf0d7532cff65ae
Michael Bissinger <Michael.Bissinger@inb.de>
2019-07-09 13:47:36 +0200

    create variables for elements and write references to them

--- app/src/main/java/example/hsport/timetracking/MainActivity.java
+++ app/src/main/java/example/hsport/timetracking/MainActivity.java
@@ -2,12 +2,33 @@ package example.hsport.timetracking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText _startDateTime;
+    private EditText _endDateTime;
+    private Button _startCommand;
+    private Button _endCommand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get references to the layout-elements
+        _startDateTime = findViewById(R.id.StartDateTime);
+        _endDateTime = findViewById(R.id.EndDateTime);
+        _startCommand = findViewById(R.id.StartCommand);
+        _endCommand = findViewById(R.id.EndCommand);
+
+        _startDateTime.setText(Calendar.getInstance().getTime().toString());
+
+
+
+
     }
 }
