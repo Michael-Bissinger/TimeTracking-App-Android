fb3ec5a3f47975f310b2af01264237ac18de5e42
Michael Bissinger <Michael.Bissinger@inb.de>
2019-07-10 16:13:18 +0200

    setup constants for use in class and initialize database

--- app/src/main/java/example/hsport/timetracking/db/DbHelper.java
+++ app/src/main/java/example/hsport/timetracking/db/DbHelper.java
@@ -4,17 +4,30 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 public class DbHelper  extends SQLiteOpenHelper {
-    public DbHelper(@Nullable Context context,
-                    @Nullable String name,
-                    @Nullable SQLiteDatabase.CursorFactory factory,
-                    int version) {
-        super(context, name, factory, version);
+
+    private static final String _DB_FILE_NAME = "time_tracking.db";
+    private static final int _DB_VERSION = 1;
+    private static final String _CREATE_TABLE = "CREATE TABLE \"time_data\" (\n" +
+            "\t\"_id\"\tINTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+            "\t\"start_time\"\tTEXT NOT NULL,\n" +
+            "\t\"end_time\"\tTEXT\n" +
+            ")";
+
+
+
+    public DbHelper(@Nullable Context context) {
+        super(context, _DB_FILE_NAME, null, _DB_VERSION);
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        Log.println(1,"Db created", "Db created");
+        db.execSQL(_CREATE_TABLE);
 
     }
 
