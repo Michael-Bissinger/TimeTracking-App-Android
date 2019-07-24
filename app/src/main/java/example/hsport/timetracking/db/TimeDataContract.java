b4d9c10e6841d91b056b63f892b5aa01aec03ae2
Michael Bissinger <Michael.Bissinger@inb.de>
2019-07-24 16:26:15 +0200

    create innerclass TimeData and give it constants for Content Provider

--- app/src/main/java/example/hsport/timetracking/db/TimeDataContract.java
+++ app/src/main/java/example/hsport/timetracking/db/TimeDataContract.java
@@ -1,10 +1,29 @@
 package example.hsport.timetracking.db;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 
 public final class TimeDataContract {
 
-    public static final String AUTHORITY = "example.hsport.timetracking"; // Is this really correct???
+    public static final String AUTHORITY = "example.hsport.timetracking.provider"; // Is this really correct???
     public static final Uri AUTHORITY_URI = Uri.parse("content://" + AUTHORITY);
+
+
+    public static final class TimeData {
+
+        // directory for data
+        public static final String CONTENT_DIRECTORY = "time";
+
+        // Uri for the data
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, CONTENT_DIRECTORY);
+
+        // Datatype for listing of Data
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_DIRECTORY;
+
+        // Datatype for a single set of data
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_DIRECTORY;
+
+    }
+
 
 }
